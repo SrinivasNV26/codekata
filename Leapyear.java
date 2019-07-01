7c9ddd9f08f4eaef92ce0eec8f11c20d415c80f1
SrinivasNV26 <52366772+SrinivasNV26@users.noreply.github.com>
2019-07-01 19:13:32 +0530

    leap year or not

---
+++ Leapyear.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Leapyear {
+	public static void main(String[] args) {
+		Scanner in= new Scanner(System.in);
+		int year;
+		System.out.println("enter the year :");
+		year=in.nextInt();
+		
+		if((year%4==0)||(year%400==0)||(year%100==0))
+		{
+			System.out.println("Yes");
+		}
+		else
+		{
+			System.out.println("No");
+		}
+	}
+
+}
