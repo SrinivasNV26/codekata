b13b6cb3208d7b96c9235bc02678b6b67d82878b
SrinivasNV26 <52366772+SrinivasNV26@users.noreply.github.com>
2019-07-01 19:26:49 +0530

    print hello

---
+++ printhello.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class printhello {
+public static void main(String[] args) {
+	Scanner in= new Scanner(System.in);
+	System.out.println("enter the value:");
+	int num=in.nextInt();
+	for(int i=0;i<num;i++)
+	{
+		System.out.println("Hello");
+	}
+}
+}
