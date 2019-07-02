d5727653fba54fa023a8c8040fcc29b2f52a16c1
SrinivasNV26 <52366772+SrinivasNV26@users.noreply.github.com>
2019-07-02 21:24:12 +0530

    odd numbers between the interval

---
+++ oddnumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class oddnumber {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int start=in.nextInt();
+		int s=in.nextInt();
+		for(int i=start+1;i<=s;i++)
+		{
+			if(i%2!=0)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+
+}
