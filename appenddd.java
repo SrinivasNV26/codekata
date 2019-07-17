f123e30d23e475a526728a1980eb8642b44a695c
SrinivasNV26 <52366772+SrinivasNV26@users.noreply.github.com>
2019-07-17 21:10:56 +0530

    append a fulstop at the dnd

---
+++ appenddd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class appenddd {
+	public static void main(String[] args) {
+		Scanner in= new Scanner(System.in);
+		String a =in.nextLine();
+		StringBuilder input=new StringBuilder();
+		input.append(a);
+		input.append(".");
+		
+		System.out.println(input);
+		
+	}
+
+}
